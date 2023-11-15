57eb13409a8a511b861eb22444841a31ac642b25
LuisJ <rabadanLjesus13@gmail.com>
2023-11-15 09:48:27 +0100

    Segundo Commit(2)

---
+++ ElTiempo_Espania/src/Modelo/Ciudades.java
@@ -0,0 +1,29 @@
+package Modelo;
+
+import java.util.List;
+
+public class Ciudades {
+
+	List <Ciudad> ciudades;
+
+	public Ciudades(List<Ciudad> ciudades) {
+		//super();
+		this.ciudades = ciudades;
+	}
+
+	public List<Ciudad> getCiudades() {
+		return ciudades;
+	}
+
+	public void setCiudades(List<Ciudad> ciudades) {
+		this.ciudades = ciudades;
+	}
+
+	@Override
+	public String toString() {
+		return "Ciudades [ciudades=" + ciudades + "]";
+	}
+	
+	
+	
+}
